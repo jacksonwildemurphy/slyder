c39eb95d195bf0097d609c9893e0c3ae1b5308d8
Jackson Murphy <jacksonmurphy1@gmail.com>
2015-02-08 12:04:37 -0700

    Made navbar static with an absolute position

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,37 +1,42 @@
+jQuery(document).ready(function () {
 
-jQuery(document).ready(function() {
-	
     /*
 	    Top menu
-	*/
-	$('.show-menu a, .hide-menu a').tooltip();
-	// show/hide menu
-	$('.show-menu a').on('click', function(e) {
-		e.preventDefault();
-		$(this).fadeOut(100, function(){ $('nav').slideDown(); });
-	});
-	$('.hide-menu a').on('click', function(e) {
-		e.preventDefault();
-		$('nav').slideUp(function(){ $('.show-menu a').fadeIn(); });
-	});
-	// navigation
-	$('nav a').on('click', function(e) {
-		e.preventDefault();
-		var element_class = $(this).attr('class');
-		var scroll_to = 0;
-		var nav_height = $('nav').height();
-		if(element_class == 'menu-top') { scroll_to = $(".coming-soon").offset().top; }
-		else if(element_class == 'menu-subscribe') { scroll_to = $(".subscribe").offset().top - nav_height - 60; }
-		else if(element_class == 'menu-project') { scroll_to = $(".about").offset().top - nav_height - 60; }
-		else if(element_class == 'menu-testimonials') { scroll_to = $(".testimonials").offset().top - nav_height - 60; }
-		else if(element_class == 'menu-about-us') { scroll_to = $(".whos-behind").offset().top - nav_height - 60; }
-		else if(element_class == 'menu-contact') { scroll_to = $(".contact").offset().top - nav_height - 60; }
-		
-		if($(window).scrollTop() != scroll_to && element_class !== undefined) {
-			$('html, body').animate({scrollTop: scroll_to}, 1000);
-		}
-	});
-	
+    */
+
+    // Makes the navbar visible once the site loads. Ideally, the navbar would just be static, but
+    // this bootstrap theme originally had a hide/show navbar feature. I chose to use this work-around
+    // so that the theme's navigation scrolling can still be used as-is. 
+    $('nav').slideDown();
+
+
+    // navigation
+    $('nav a').on('click', function (e) {
+        e.preventDefault();
+        var element_class = $(this).attr('class');
+        var scroll_to = 0;
+        var nav_height = $('nav').height();
+        if (element_class == 'menu-top') {
+            scroll_to = $(".coming-soon").offset().top;
+        } else if (element_class == 'menu-subscribe') {
+            scroll_to = $(".subscribe").offset().top - nav_height - 60;
+        } else if (element_class == 'menu-project') {
+            scroll_to = $(".about").offset().top - nav_height - 60;
+        } else if (element_class == 'menu-testimonials') {
+            scroll_to = $(".testimonials").offset().top - nav_height - 60;
+        } else if (element_class == 'menu-about-us') {
+            scroll_to = $(".whos-behind").offset().top - nav_height - 60;
+        } else if (element_class == 'menu-contact') {
+            scroll_to = $(".contact").offset().top - nav_height - 60;
+        }
+
+        if ($(window).scrollTop() != scroll_to && element_class !== undefined) {
+            $('html, body').animate({
+                scrollTop: scroll_to
+            }, 1000);
+        }
+    });
+
     /*
         Background slideshow
     */
@@ -39,37 +44,40 @@ jQuery(document).ready(function() {
       "assets/img/backgrounds/1.jpg"
     , "assets/img/backgrounds/2.jpg"
     , "assets/img/backgrounds/3.jpg"
-    ], {duration: 3000, fade: 750});
-    
+    ], {
+        duration: 3000,
+        fade: 750
+    });
+
     $('.about-container').backstretch("assets/img/backgrounds/2.jpg");
-    
+
     $('.whos-behind-container').backstretch("assets/img/backgrounds/4.jpg");
 
-    
+
     // Countdown timer code went here...
-    
+
     /*
         Testimonials
     */
     $('.testimonial-active').html('<p>' + $('.testimonial-single:first p').html() + '</p>');
     $('.testimonial-single:first .testimonial-single-image img').css('opacity', '1');
-    
-    $('.testimonial-single-image img').on('click', function() {
-    	$('.testimonial-single-image img').css('opacity', '0.5');
-    	$(this).css('opacity', '1');
-    	var new_testimonial_text = $(this).parent('.testimonial-single-image').siblings('p').html();
-    	$('.testimonial-active p').fadeOut(300, function() {
-    		$(this).html(new_testimonial_text);
-    		$(this).fadeIn(400);
-    	});
+
+    $('.testimonial-single-image img').on('click', function () {
+        $('.testimonial-single-image img').css('opacity', '0.5');
+        $(this).css('opacity', '1');
+        var new_testimonial_text = $(this).parent('.testimonial-single-image').siblings('p').html();
+        $('.testimonial-active p').fadeOut(300, function () {
+            $(this).html(new_testimonial_text);
+            $(this).fadeIn(400);
+        });
     });
-    
+
     /*
 	    Show latest tweets
 	*/
-	
+
     /* Omit the tweet feature for now... 
-    
+
     $('.latest-tweets .tweets').tweet({
 		modpath: 'assets/twitter/',
 		username: 'anli_zaimi',
@@ -97,16 +105,22 @@ jQuery(document).ready(function() {
     	});
     	clicked_tweet_nav.css('background', '#e8643e');
     });
-    
+
     */
 
     /*
 	    Google maps
 	*/
     var position = new google.maps.LatLng(40.767451, -111.882151);
-    $('.contact-address .map').gmap({'center': position, 'zoom': 13, 'disableDefaultUI':true, 'callback': function() {
+    $('.contact-address .map').gmap({
+        'center': position,
+        'zoom': 13,
+        'disableDefaultUI': true,
+        'callback': function () {
             var self = this;
-            self.addMarker({'position': this.get('map').getCenter() });	
+            self.addMarker({
+                'position': this.get('map').getCenter()
+            });
         }
     });
 
@@ -116,22 +130,21 @@ jQuery(document).ready(function() {
     $('.success-message').hide();
     $('.error-message').hide();
 
-    $('.subscribe form').submit(function(e) {
-    	e.preventDefault();
+    $('.subscribe form').submit(function (e) {
+        e.preventDefault();
         var postdata = $('.subscribe form').serialize();
         $.ajax({
             type: 'POST',
             url: 'assets/subscribe.php',
             data: postdata,
             dataType: 'json',
-            success: function(json) {
-                if(json.valid == 0) {
+            success: function (json) {
+                if (json.valid == 0) {
                     $('.success-message').hide();
                     $('.error-message').hide();
                     $('.error-message').html(json.message);
                     $('.error-message').fadeIn();
-                }
-                else {
+                } else {
                     $('.error-message').hide();
                     $('.success-message').hide();
                     $('.subscribe form').hide();
@@ -141,41 +154,40 @@ jQuery(document).ready(function() {
             }
         });
     });
-    
+
     /*
 	    Contact form
 	*/
-    $('.contact-form form input[type="text"], .contact-form form textarea').on('focus', function() {
-    	$('.contact-form form input[type="text"], .contact-form form textarea').removeClass('contact-error');
+    $('.contact-form form input[type="text"], .contact-form form textarea').on('focus', function () {
+        $('.contact-form form input[type="text"], .contact-form form textarea').removeClass('contact-error');
     });
-	$('.contact-form form').submit(function(e) {
-		e.preventDefault();
-	    $('.contact-form form input[type="text"], .contact-form form textarea').removeClass('contact-error');
-	    var postdata = $('.contact-form form').serialize();
-	    $.ajax({
-	        type: 'POST',
-	        url: 'assets/contact.php',
-	        data: postdata,
-	        dataType: 'json',
-	        success: function(json) {
-	            if(json.emailMessage != '') {
-	                $('.contact-form form .contact-email').addClass('contact-error');
-	            }
-	            if(json.subjectMessage != '') {
-	                $('.contact-form form .contact-subject').addClass('contact-error');
-	            }
-	            if(json.messageMessage != '') {
-	                $('.contact-form form textarea').addClass('contact-error');
-	            }
-	            if(json.emailMessage == '' && json.subjectMessage == '' && json.messageMessage == '') {
-	                $('.contact-form form').fadeOut('fast', function() {
-	                    $('.contact-form').append('<p>Thanks for contacting us! We will get back to you very soon.</p>');
-	                });
-	            }
-	        }
-	    });
-	});
+    $('.contact-form form').submit(function (e) {
+        e.preventDefault();
+        $('.contact-form form input[type="text"], .contact-form form textarea').removeClass('contact-error');
+        var postdata = $('.contact-form form').serialize();
+        $.ajax({
+            type: 'POST',
+            url: 'assets/contact.php',
+            data: postdata,
+            dataType: 'json',
+            success: function (json) {
+                if (json.emailMessage != '') {
+                    $('.contact-form form .contact-email').addClass('contact-error');
+                }
+                if (json.subjectMessage != '') {
+                    $('.contact-form form .contact-subject').addClass('contact-error');
+                }
+                if (json.messageMessage != '') {
+                    $('.contact-form form textarea').addClass('contact-error');
+                }
+                if (json.emailMessage == '' && json.subjectMessage == '' && json.messageMessage == '') {
+                    $('.contact-form form').fadeOut('fast', function () {
+                        $('.contact-form').append('<p>Thanks for contacting us! We will get back to you very soon.</p>');
+                    });
+                }
+            }
+        });
+    });
 
-    
+
 });
-
